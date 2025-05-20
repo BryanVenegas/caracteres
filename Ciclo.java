ee765232c4150543bfbf71f4ee89ecf2db052693
BryanVenegas <20243rd060@utez.edu.mx>
2025-05-20 11:47:14 -0600

    se cambia la funcion

--- Ciclo.java
+++ Ciclo.java
@@ -1,24 +1,26 @@
-
+import java.util.Scanner;
 public class Ciclo {
 private static final Scanner scanner = new Scanner(System.in);
 
 public static int obtenerNumero() {
     System.out.print("Introduce un número: ");
+    int numero = scanner.nextInt();
+    while (numero <0) {
+        System.out.print("El número debe ser mayor a 0. Inténtalo de nuevo: ");
+        numero = scanner.nextInt();
+
+    }
     return scanner.nextInt();
 }
 public static void main(String[] args) {
-    int numero = obtenerNumero();
+    int num = obtenerNumero();
 
-    while (numero < 0) {
-        numero = obtenerNumero();
-    }
 
-    System.out.println("Fin del programa");
+    System.out.println("tu numero es :"+num);
 
 
 
 
-
-
+}
 
 }
